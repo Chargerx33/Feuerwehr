218c41cf71d765157d8c6831347ad6138220ed22
Charger X33 <66776191+Chargerx33@users.noreply.github.com>
2023-11-22 09:52:38 +0100

    Einsatz Erstellen (mit Art und Nummer)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         Leitstelle leitstelle = new Leitstelle();
-        leitstelle.erkrankung(0);
+        leitstelle.createEinsatz();
         /*Test1 dialog = new Test1();
         dialog.pack();
         dialog.setVisible(true);
