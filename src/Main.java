b526417a2559708a8c56f8dc65c034723c74761c
Charger X33 <66776191+Chargerx33@users.noreply.github.com>
2023-11-30 11:16:58 +0100

    Einsatz Erstellen (mit Art und Nummer)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Leitstelle leitstelle = new Leitstelle();
         leitstelle.erkrankung(0);
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.pack();
+        mainWindow.setVisible(true);
         /*Test1 dialog = new Test1();
         dialog.pack();
         dialog.setVisible(true);
         System.exit(0);*/
         System.out.println(1);
     }
-
 
 }
---
+++ src/MainWindow.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MainWindow extends JDialog {
+    private JPanel contentPane;
+    private JButton w;
+    private JButton buttonCancel;
+
+    public MainWindow() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(w);
+
+        w.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        MainWindow dialog = new MainWindow();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
